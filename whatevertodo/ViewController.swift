e2ed39c2060680438ed233f21edcd26df03461eb
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2018-06-19 15:40:22 +0800

    Initial Commit

---
+++ whatevertodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatevertodo
+//
+//  Created by JiaChen(: on 19/6/18.
+//  Copyright © 2018 SST Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
